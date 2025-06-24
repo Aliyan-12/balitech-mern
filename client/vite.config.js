33f32990f795c3667df2766b69c67318c07cceba
Aliyan-12 <alyannasir225@gmail.com>
2025-06-24 22:35:43 +0500

    origin changed.

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['localhost', '127.0.0.1', 'client.balitech.org', 'server.balitech.org'],
+    allowedHosts: ['localhost', '127.0.0.1', 'balitech.org', 'app.balitech.org'],
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
